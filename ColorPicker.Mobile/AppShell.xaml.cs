cb6afc36fd4c5aeb95b8708006c4eea2107802f6
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-07-17 00:39:33 +0100

    ColorPicker project

---
+++ ColorPicker.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ColorPicker.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ColorPicker.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ColorPicker.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
